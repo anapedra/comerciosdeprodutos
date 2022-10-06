e996fa3106288885fd00fa15da27888228b7f30f
AnaSantana <anapedra.mil@gmail.com>
2022-10-06 15:33:13 -0300

    Initial commit

---
+++ src/main/java/com/santanaanalopess/comerciosdeprodutos/ComerciosdeprodutosApplication.java
@@ -0,0 +1,13 @@
+package com.santanaanalopess.comerciosdeprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComerciosdeprodutosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComerciosdeprodutosApplication.class, args);
+    }
+
+}
